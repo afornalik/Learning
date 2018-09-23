fb427a40fcf3d5ceba7779f726e7e443e5f36823
Andrzejf <afornalik2@gmail.com>
2018-09-23 22:53:59 +0200

    Changes in GetOrder class.

--- src/main/java/com/testapp/afor/class1/GetOrder.java
+++ src/main/java/com/testapp/afor/class1/GetOrder.java
@@ -1,6 +1,14 @@
 package main.java.com.testapp.afor.class1;
 
+import java.util.Date;
+
 public class GetOrder {
 
 
+    Date orderDate = new Date();
+    ArrayList<book> book = new ArrayList<book>;
+
+    public GetOrder() {
+
+    }
 }
